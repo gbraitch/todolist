4853bcf247bd67518def63d77589cbd79aa5af3f
gurse16 <q4v0b@ugrad.cs.ubc.ca>
2019-07-06 22:36:23 -0700

    Create more modular functions for each todo function i.e add, del, view. Added delete a todo item option

--- src/ui/TodoList.java
+++ src/ui/TodoList.java
@@ -15,34 +15,50 @@ public class TodoList {
             String choice = scanner.nextLine();
             switch (choice) {
                 case "0":
-                    System.out.println("Choose a name for the todo!");
-                    String newTodoName = scanner.nextLine();
-                    System.out.println("Choose a due date for the todo!");
-                    String newTodoDue = scanner.nextLine();
-                    list.add(new Todo (newTodoName, newTodoDue));
+                    addTodo();
                     break;
                 case "1":
-                    System.out.println("You chose 1");
+                    deleteTodo();
                     break;
                 case "2":
                     System.out.println("You chose 2");
                     break;
                 case "3":
-                    int i = 0;
-                    for (Todo td : list)
-                    {
-                        System.out.print("["+i+"]  ");
-                        System.out.println(td.getName() + "   :   " + td.getDue());
-                        i++;
-                    }
+                    printTodoList();
                     scanner.nextLine();
                     break;
             }
         }
     }
 
-    public static void printMenu(){
-        System.out.println("Enter 0: Create new Todo");
+    public void addTodo(){
+        System.out.println("Choose a name for the todo!");
+        String newTodoName = scanner.nextLine();
+        System.out.println("Choose a due date for the todo!");
+        String newTodoDue = scanner.nextLine();
+        list.add(new Todo (newTodoName, newTodoDue));
+    }
+
+    public void deleteTodo(){
+        printTodoList();
+        System.out.println("\nWhich Todo to remove?");
+        String del = scanner.nextLine();
+        list.remove(Integer.parseInt(del));
+    }
+
+    public void printTodoList(){
+        System.out.println();
+        int i = 0;
+        for (Todo td : list)
+        {
+            System.out.print("["+i+"]  ");
+            System.out.println(td.getName() + "   :   " + td.getDue());
+            i++;
+        }
+    }
+
+    public void printMenu(){
+        System.out.println("\nEnter 0: Create new Todo");
         System.out.println("      1: Delete Todo");
         System.out.println("      2: Edit Todo");
         System.out.println("      3: View Todo List");
