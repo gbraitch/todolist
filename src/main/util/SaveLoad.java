68c4240de2914524b771aab811deb92ae7bcfc33
gurse16 <q4v0b@ugrad.cs.ubc.ca>
2019-07-28 19:49:58 -0700

    small edit

--- src/main/util/SaveLoad.java
+++ src/main/util/SaveLoad.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
 import model.Todo;
-import ui.InterfaceAdapter;
 
 import java.io.FileReader;
 import java.io.FileWriter;
